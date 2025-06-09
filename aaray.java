2a73541b65f62a1bf19d0ade2474ef8ddcd232fb
AayushB3432 <aayushbhagwat7199@gmail.com>
2025-06-09 11:05:16 +0530

    Create aaray.java

---
+++ aaray.java
@@ -0,0 +1,15 @@
+public class array {
+    public static void main(String[] args) 
+    {
+        int [] number = new int[3];
+        number[0]=12;
+        number[1]=13;
+        number[2]=14;
+        for(int i=0;i<number.length;i++)
+        {
+            System.out.println(number[i]);
+
+        }
+    }
+    
+}
