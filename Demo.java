ab12e179872bbef0f2a82214569ea44529ddfd62
AayushB3432 <aayushbhagwat7199@gmail.com>
2025-06-09 11:07:48 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo
+{
+    public static void main(String[] args)
+    {
+        int num1=9;
+        if(num1%2==0)
+        {
+            System.out.println("the no. is even");
+        }
+        else
+        {
+            System.out.println("the no is odd");
+        }
+    }
+}
